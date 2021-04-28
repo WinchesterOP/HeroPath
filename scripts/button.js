427c6d9b4138755039a92fdfc003215a0baa60d4
Neco Giedrojc <neco.giedrojc@gmail.com>
2021-04-28 18:32:13 +0200

    changing tabs added

--- scripts/button.js
+++ scripts/button.js
@@ -1,5 +1,8 @@
 let button_func = {
 
+    first_area_block: document.getElementById("first_area"),
+    home_area_block: document.getElementById("home_area"),
+
     clickedOnRecoveryButton: function() {
         logging("INFO", "Recovery-Button has been pressed");
         progressbar.recover_stamina();
@@ -23,6 +26,26 @@ let button_func = {
     clickedOnReinforceBodyButton: function() {
         logging("INFO", "ReinforceBody-Button has been pressed");
         jobs.reinforceBody();
+    },
+
+    clickedOnLittleTownButton: function() {
+        if (button_func.first_area_block.style.display === "none") {
+            button_func.first_area_block.style.display = "block";
+            button_func.home_area_block.style.display = "none";
+        } else {
+            button_func.first_area_block.style.display = "none";
+            button_func.home_area_block.style.display = "block";
+        }
+    },
+
+    clickedOnHomeButton: function() {
+        if (button_func.home_area_block.style.display === "none") {
+            button_func.home_area_block.style.display = "block";
+            button_func.first_area_block.style.display = "none";
+        } else {
+            button_func.home_area_block.style.display = "none";
+            button_func.first_area_block.style.display = "block";
+        }
     },
 
 };
