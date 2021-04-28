6bc182907b031c5a4434a8d68b5ccedd68a5c202
Neco Giedrojc <neco.giedrojc@gmail.com>
2021-04-28 09:02:24 +0200

    added tooltip

---
+++ scripts/tooltip.js
@@ -0,0 +1,4 @@
+tippy('#reinforceBodyButton', {
+    content: 'Reinforce Button \n Cost: 10 Mana \n Effect: \n HealthMax: +10 ',
+    content: 'Reinforce Button \n Cost: 10 Mana \n Effect: \n HealthMax: +10 ',
+});
