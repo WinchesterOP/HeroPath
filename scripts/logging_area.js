13203526156367ca5dd61b6c87d59fee02c0d3ab
Neco Giedrojc <neco.giedrojc@gmail.com>
2021-04-28 13:55:33 +0200

    linebreak for logging area

--- scripts/logging_area.js
+++ scripts/logging_area.js
@@ -2,7 +2,7 @@ let logging_area = {
 
     logSomeText: function(textToAdd) {
         let loggingText = document.getElementById("logging_area_text");
-        loggingText.innerHTML += textToAdd;
+        loggingText.innerHTML += textToAdd + '<br>';
     },
 
 };
